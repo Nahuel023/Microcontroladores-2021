3930e68783cd735650c4acc354b9b027cd54ca2f
Nahuel Medina <43505973+Nahuel023@users.noreply.github.com>
2021-08-18 15:24:56 -0300

    Add files via upload
    Proyecto vacío, encendido y apagado de un led

---
+++ LedArduinoUno/LedArduinoUno/main.c
@@ -0,0 +1,19 @@
+/*
+ * LedArduinoUno.c
+ *
+ * Created: 17/8/2021 18:23:27
+ * Author : Nahuel Medina
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	DDRD = 0xFF
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
